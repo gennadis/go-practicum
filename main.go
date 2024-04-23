ac4d7f557fa5d7c75f1a0e097500e11053c39753
Gennady Badinov <ggbadinov@cloud.ru>
2024-04-23 16:21:33 +0300

    feat: truncate current time to a day start

--- main.go
+++ main.go
@@ -2,22 +2,11 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"time"
 )
 
 func main() {
-	currentTimeStr := "2021-09-19T15:59:41+03:00"
-	layout := time.RFC3339
-	currentTime, err := time.Parse(layout, currentTimeStr)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(currentTime)
-
 	now := time.Now()
-
-	fmt.Println("Is", now, "before", currentTime, "? Answer:", now.Before(currentTime))
-	fmt.Println("Is", now, "after", currentTime, "? Answer:", now.After(currentTime))
-	fmt.Println("Is", now, "equal", currentTime, "? Answer:", now.Equal(currentTime))
+	truncTime := now.Truncate(time.Hour * 24)
+	fmt.Println(truncTime)
 }
