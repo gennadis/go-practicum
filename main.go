e501186ea1a8585ae5f64a148e93ca0d0ac4f9de
Gennady Badinov <ggbadinov@cloud.ru>
2024-04-23 15:52:27 +0300

    feat: parse time from string

--- main.go
+++ main.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"time"
 )
 
 func main() {
-	now := time.Now()
-	fmt.Println(now.Format(time.RFC1123)) // Sun, 19 Sep 2021 15:42:00 MSK
+	currentTimeStr := "2021-09-19T15:59:41+03:00"
+	layout := "2006-01-02T15:04:05-07:00"
+	currentTime, err := time.Parse(layout, currentTimeStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(currentTime)
 }
