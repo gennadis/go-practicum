6b2b9fadd82be0eb0c86ddeb5b3a08c59baee766
Gennady Badinov <ggbadinov@cloud.ru>
2024-04-23 15:53:21 +0300

    feat: parse time from string using builtin layout

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	currentTimeStr := "2021-09-19T15:59:41+03:00"
-	layout := "2006-01-02T15:04:05-07:00"
+	layout := time.RFC3339
 	currentTime, err := time.Parse(layout, currentTimeStr)
 	if err != nil {
 		log.Fatal(err)
