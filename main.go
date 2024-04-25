2cc4bdfaea0e830c9889ec9f35e59f07aa1be432
Gennady Badinov <gennady.badinov@gmail.com>
2024-04-25 19:43:13 +0300

    feat: dump custom http request

--- main.go
+++ main.go
@@ -1,25 +1,45 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"io"
 	"net/http"
+	"net/http/httputil"
+	"time"
 )
 
 func main() {
-	response, err := http.Get("http://example.com")
+	client := &http.Client{}
+	client.Timeout = time.Second * 1
+	client.CheckRedirect = func(req *http.Request, via []*http.Request) error {
+		if len(via) >= 2 {
+			return errors.New("Stopped after 2 redirects")
+		}
+		return nil
+	}
+
+	request, err := http.NewRequest(http.MethodGet, "https://research.swtch.com/interfaces", nil)
+	request.Header.Set("Content-Type", "application/json; charset=UTF-8")
+	request.Header.Add("Accept", "application/json")
 	if err != nil {
 		fmt.Println(err)
 	}
+	response, err := client.Do(request)
+	if err != nil {
+		fmt.Println(err)
+	}
+
 	contentType := response.Header.Get("content-type")
 
 	defer response.Body.Close()
-	payload, err := io.ReadAll(response.Body)
+
+	requestDump, err := httputil.DumpRequest(request, true)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println(err.Error())
 	}
+	fmt.Println(string(requestDump))
 
 	fmt.Println(response.Status)
 	fmt.Println(contentType)
-	fmt.Println(string(payload))
+
 }
