908d8efc118ef7e0f9d590a7e5fb8eb0f55edb5a
Gennady Badinov <gennady.badinov@gmail.com>
2024-04-27 18:55:01 +0300

    feat: run hello world server

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "net/http"
+
+func HelloWorld(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>Hello, World</h1>"))
+}
+
+func main() {
+	http.HandleFunc("/", HelloWorld)
+	http.ListenAndServe(":8080", nil)
+}
