e99957d29274450fb68cd9677093c384e116f7a2
Gennady Badinov <gennady.badinov@gmail.com>
2024-05-01 18:56:05 +0300

    feat: load config values from flags

--- internal/app/config/config.go
+++ internal/app/config/config.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"flag"
 	"os"
 )
 
@@ -11,9 +12,21 @@ type Config struct {
 }
 
 func SetConfig() Config {
-	return Config{
+	config := Config{
 		ServerAddr:      os.Getenv("SERVER_ADDRESS"),
 		BaseURL:         os.Getenv("BASE_URL"),
 		FileStoragePath: os.Getenv("FILE_STORAGE_PATH"),
 	}
+	if config.ServerAddr == "" {
+		flag.StringVar(&config.ServerAddr, "a", "localhost:8080", "server address")
+	}
+	if config.BaseURL == "" {
+		flag.StringVar(&config.BaseURL, "b", "http://localhost:8080", "base url")
+	}
+	if config.FileStoragePath == "" {
+		flag.StringVar(&config.FileStoragePath, "f", "local_storage.json", "file storage path")
+	}
+	flag.Parse()
+
+	return config
 }
