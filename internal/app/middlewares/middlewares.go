a0381aac0c61779efa5676ee13b496be69f6a288
Gennady Badinov <gennady.badinov@gmail.com>
2024-05-04 14:31:32 +0300

    feat: implement cookie auth middleware

--- internal/app/middlewares/middlewares.go
+++ internal/app/middlewares/middlewares.go
@@ -2,11 +2,21 @@ package middlewares
 
 import (
 	"compress/gzip"
-	"crypto/rand"
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"strings"
+
+	"github.com/google/uuid"
+)
+
+const (
+	cookieName = "authCookie"
+	secretKey  = "secretKeyHere"
 )
 
 type gzipWriter struct {
@@ -50,25 +60,58 @@ func GzipSenderMiddleware(next http.Handler) http.Handler {
 	})
 }
 
-func generateRandom(size int) ([]byte, error) {
-	b := make([]byte, size)
-	_, err := rand.Read(b)
-	if err != nil {
-		return nil, err
-	}
-	return b, nil
-}
-
-// Not implemented yet
 func CookieAuthMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		randBytes, err := generateRandom(16)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
+		cookie, err := r.Cookie(cookieName)
+
+		if err == http.ErrNoCookie || !isValidCookie(cookie) {
+			userID := uuid.New()
+
+			cookieValue := signCookie(userID.String())
+			newCookie := http.Cookie{
+				Name:  cookieName,
+				Value: cookieValue,
+			}
+
+			http.SetCookie(w, &newCookie)
+			log.Println("new cookie is set")
+
+			next.ServeHTTP(w, r)
 			return
 		}
-		log.Println(randBytes)
-
+		log.Println("cookie is valid")
 		next.ServeHTTP(w, r)
 	})
+}
+
+func signCookie(value string) string {
+	mac := hmac.New(sha256.New, []byte(secretKey))
+	mac.Write([]byte(value))
+	signature := mac.Sum(nil)
+
+	signedValue := append([]byte(value), signature...)
+
+	encodedValue := base64.StdEncoding.EncodeToString(signedValue)
+	fmt.Println(encodedValue)
+
+	return encodedValue
+}
+
+func isValidCookie(cookie *http.Cookie) bool {
+	if cookie == nil || cookie.Value == "" {
+		return false
+	}
+
+	decodedCookie, err := base64.StdEncoding.DecodeString(cookie.Value)
+	if err != nil || len(decodedCookie) < sha256.Size {
+		return false
+	}
+	cookieValue := decodedCookie[:len(decodedCookie)-sha256.Size]
+	hmacSignature := decodedCookie[len(decodedCookie)-sha256.Size:]
+
+	mac := hmac.New(sha256.New, []byte(secretKey))
+	mac.Write(cookieValue)
+	expectedHMAC := mac.Sum(nil)
+
+	return hmac.Equal(hmacSignature, expectedHMAC)
 }
