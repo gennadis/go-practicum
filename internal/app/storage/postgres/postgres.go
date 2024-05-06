281d0383aa74831cf090ef02713cf7ad73dac11f
Gennady Badinov <gennady.badinov@gmail.com>
2024-05-06 14:21:32 +0300

    feat: create db tables on init

--- internal/app/storage/postgres/postgres.go
+++ internal/app/storage/postgres/postgres.go
@@ -2,6 +2,7 @@ package postgres
 
 import (
 	"database/sql"
+	"log"
 
 	_ "github.com/jackc/pgx/v5/stdlib"
 )
@@ -14,6 +15,25 @@ func New(postgresDSN string) (*PostgresStore, error) {
 	db, err := sql.Open("pgx", postgresDSN)
 	if err != nil {
 		return nil, err
+	}
+	if _, err := db.Exec(`
+		CREATE TABLE IF NOT EXISTS users (
+			user_id UUID PRIMARY KEY
+		);`,
+	); err != nil {
+		log.Printf("users table creation error: %s", err)
+	}
+
+	if _, err := db.Exec(`
+		CREATE TABLE urls (
+			user_id UUID NOT NULL,
+			slug VARCHAR(255) NOT NULL,
+			original_url VARCHAR(2048) NOT NULL,
+			PRIMARY KEY (slug),
+			FOREIGN KEY (user_id) REFERENCES users(user_id)
+		);`,
+	); err != nil {
+		log.Printf("urls table creation error: %s", err)
 	}
 	return &PostgresStore{db: db}, nil
 }
