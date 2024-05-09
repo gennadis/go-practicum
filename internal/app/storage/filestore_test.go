be1adc957d780568eacaf3d661ea7cd5854b3099
Gennady Badinov <gennady.badinov@gmail.com>
2024-05-09 18:39:23 +0300

    test: inmem and file storage methods

--- internal/app/storage/filestore_test.go
+++ internal/app/storage/filestore_test.go
@@ -173,3 +173,121 @@ func TestFileStore_Ping(t *testing.T) {
 		t.Errorf("Expected ping err nil, got err %s", err)
 	}
 }
+
+func TestFileStore_AppendDataSequentially(t *testing.T) {
+	tmpfile, err := os.CreateTemp("", "testfilestore")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(tmpfile.Name())
+	tmpfile.Close()
+
+	store, err := NewFileStorage(tmpfile.Name())
+	if err != nil {
+		t.Fatalf("Error creating file store: %v", err)
+	}
+
+	data := map[string]string{
+		"key1": "value1",
+		"key2": "value2",
+		"key3": "value3",
+	}
+
+	for key, value := range data {
+		if err := store.AddURL(key, value, "userID"); err != nil {
+			t.Fatalf("Error writing to store: %v", err)
+		}
+	}
+
+	fileContent, err := os.Open(tmpfile.Name())
+	if err != nil {
+		t.Fatalf("Error reading file content: %v", err)
+	}
+
+	var fileData map[string]map[string]string
+	decoder := json.NewDecoder(fileContent)
+	if err := decoder.Decode(&fileData); err != nil {
+		t.Fatalf("Error decoding JSON: %v", err)
+	}
+
+	for key, expectedValue := range data {
+		if userURLs, ok := fileData["userID"]; ok {
+			if value, ok := userURLs[key]; !ok || value != expectedValue {
+				t.Errorf("Expected value %s for key %s, got %s", expectedValue, key, value)
+			}
+		} else {
+			t.Errorf("Expected userID map not found in file data")
+		}
+	}
+
+	additionalData := map[string]string{
+		"key4": "value4",
+		"key5": "value5",
+	}
+
+	for key, value := range additionalData {
+		if err := store.AddURL(key, value, "userID"); err != nil {
+			t.Fatalf("Error writing to store: %v", err)
+		}
+	}
+
+	_, _ = fileContent.Seek(0, 0)
+	decoder = json.NewDecoder(fileContent)
+	if err := decoder.Decode(&fileData); err != nil {
+		t.Fatalf("Error decoding JSON: %v", err)
+	}
+
+	for key, expectedValue := range additionalData {
+		if userURLs, ok := fileData["userID"]; ok {
+			if value, ok := userURLs[key]; !ok || value != expectedValue {
+				t.Errorf("Expected value %s for key %s, got %s", expectedValue, key, value)
+			}
+		} else {
+			t.Errorf("Expected userID map not found in file data")
+		}
+	}
+}
+
+func TestFileStore_AddURL_ExistingURL(t *testing.T) {
+	tmpfile, err := os.CreateTemp("", "testfilestore")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(tmpfile.Name())
+	tmpfile.Close()
+
+	store, err := NewFileStorage(tmpfile.Name())
+	if err != nil {
+		t.Fatalf("Error creating file store: %v", err)
+	}
+
+	key := "key1"
+	value := "value1"
+	if err := store.AddURL(key, value, "userID"); err != nil {
+		t.Fatalf("Error adding URL: %v", err)
+	}
+
+	if err := store.AddURL(key, value, "userID"); err != ErrorURLAlreadyExists {
+		t.Errorf("Expected ErrorURLAlreadyExists, got: %v", err)
+	}
+}
+
+func TestFileStorage_Ping(t *testing.T) {
+	tmpfile, err := os.CreateTemp("", "testfilestore")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(tmpfile.Name())
+	tmpfile.Close()
+
+	store, err := NewFileStorage(tmpfile.Name())
+	if err != nil {
+		t.Fatalf("Error creating file store: %v", err)
+	}
+
+	err = store.Ping()
+
+	if err != nil {
+		t.Errorf("Expected nil error, got: %v", err)
+	}
+}
--- internal/app/storage/memstore_test.go
+++ internal/app/storage/memstore_test.go
@@ -94,3 +94,65 @@ func TestMemStore_Ping(t *testing.T) {
 		t.Errorf("Expected ping err nil, got err %s", err)
 	}
 }
+
+func TestMemStore_BatchAddURLs(t *testing.T) {
+	store := NewMemoryStorage()
+
+	batch := []BatchURLsElement{
+		{Slug: "key1", OriginalURL: "https://example1.com"},
+		{Slug: "key2", OriginalURL: "https://example2.com"},
+	}
+
+	userID := "userID"
+
+	if err := store.BatchAddURLs(batch, userID); err != nil {
+		t.Fatalf("Error in BatchAddURLs: %v", err)
+	}
+
+	for _, element := range batch {
+		url, err := store.GetURL(element.Slug, userID)
+		if err != nil {
+			t.Fatalf("Error getting URL for slug %s: %v", element.Slug, err)
+		}
+		if url != element.OriginalURL {
+			t.Errorf("Expected URL %s for slug %s, got %s", element.OriginalURL, element.Slug, url)
+		}
+	}
+}
+
+func TestMemStore_GetSlugByOriginalURL(t *testing.T) {
+	store := NewMemoryStorage()
+
+	data := map[string]string{
+		"key1": "https://example1.com",
+		"key2": "https://example2.com",
+	}
+
+	userID := "userID"
+
+	for slug, url := range data {
+		if err := store.AddURL(slug, url, userID); err != nil {
+			t.Fatalf("Error adding URL %s: %v", url, err)
+		}
+	}
+
+	for _, url := range data {
+		slug, err := store.GetSlugByOriginalURL(url, userID)
+		if err != nil {
+			t.Fatalf("Error getting slug for URL %s: %v", url, err)
+		}
+		if _, ok := data[slug]; !ok {
+			t.Errorf("Expected slug for URL %s, got %s", url, slug)
+		}
+	}
+}
+
+func TestMemoryStorage_Ping(t *testing.T) {
+	store := NewMemoryStorage()
+
+	err := store.Ping()
+
+	if err != nil {
+		t.Errorf("Expected nil error, got: %v", err)
+	}
+}
