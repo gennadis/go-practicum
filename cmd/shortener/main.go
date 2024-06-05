ff006034cdde9d9779212baadcfed0abea77736e
Gennady Badinov <gennady.badinov@gmail.com>
2024-06-05 20:17:29 +0300

    feat: set build version, date, and commit at compile time (#20)

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -3,6 +3,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net/http"
 
@@ -10,8 +11,24 @@ import (
 	"github.com/gennadis/shorturl/internal/app/config"
 )
 
+// To set buildVersion, buildDate, and buildCommit at compile time, use the
+// `-ldflags` option with go run or go build. This allows embedding version
+// information directly into the binary. By default, these values are set to "N/A".
+// Example:
+// go run -ldflags "-X main.buildVersion=0.1.0 -X 'main.buildDate=$(date +'%Y/%m/%d %H:%M:%S')' -X main.buildCommit=30161ae" cmd/shortener/main.go
+var (
+	buildVersion = "N/A"
+	buildDate    = "N/A"
+	buildCommit  = "N/A"
+)
+
 // main is the entry point for the application.
 func main() {
+	// Print buildVersion, buildDate, and buildCommit on startup
+	fmt.Println(buildVersion)
+	fmt.Println(buildDate)
+	fmt.Println(buildCommit)
+
 	// Create a new background context.
 	ctx := context.Background()
 
